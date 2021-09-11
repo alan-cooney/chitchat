ea5e62435893c84b21236599f7f39bf57b8c2002
Alan Cooney <alan@skyhookadventure.com>
2021-09-11 16:07:19 +0100

    Simplify cdk deploy

--- backend/src/bin/index.ts
+++ backend/src/bin/index.ts
@@ -1,18 +1,10 @@
 import { App, Tags } from "@aws-cdk/core";
 import BackendStack from "../lib/BackendStack";
 
-// Check the stage is specified
-if (!process.env.STAGE) {
-  throw Error(
-    "Please specify a stage, e.g. with STAGE=Dev yarn cdk deploy.\n\nYou can also use yarn deployDev."
-  );
-}
-
 // Create the app/stack
 const app = new App();
-export const stackName = `chitchat-${process.env.STAGE}`;
+export const stackName = `chitchat`;
 const stack = new BackendStack(app, stackName);
 
 // Add billing tags
 Tags.of(stack).add("Service", stackName);
-Tags.of(stack).add("Stage", process.env.STAGE);
