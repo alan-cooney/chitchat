f410fe6851b8906cdeff56f238b8b45050162362
Alan Cooney <alan@skyhookadventure.com>
2021-09-11 11:27:35 +0100

    Enable deploy-per-PR on the backend
    This requires naming using the env variable STAGE, so that we can name the stack based on the PR name.

--- backend/src/bin/index.ts
+++ backend/src/bin/index.ts
@@ -1,9 +1,18 @@
 import { App, Tags } from "@aws-cdk/core";
 import BackendStack from "../lib/BackendStack";
 
-const app = new App();
+// Check the stage is specified
+if (!process.env.STAGE) {
+  throw Error(
+    "Please specify a stage, e.g. with STAGE=Dev yarn cdk deploy.\n\nYou can also use yarn deployDev."
+  );
+}
 
-// eslint-disable-next-line import/prefer-default-export
-export const stackName = "serverless-chat";
+// Create the app/stack
+const app = new App();
+export const stackName = `chitchat-${process.env.STAGE}`;
 const stack = new BackendStack(app, stackName);
+
+// Add billing tags
 Tags.of(stack).add("Service", stackName);
+Tags.of(stack).add("Stage", process.env.STAGE);
